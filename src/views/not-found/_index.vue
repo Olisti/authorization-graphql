07452c5b2abac6d084aa860e8bb47a06815a29b2
Svetlana <gerbophilia@gmail.com>
2020-09-24 20:41:18 +0300

    404 page

--- src/views/not-found/_index.vue
+++ src/views/not-found/_index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>404</div>
+    <h1>404</h1>
 </template>
 
 <script lang="ts">
@@ -9,4 +9,15 @@ import { Component, Vue } from 'vue-property-decorator';
 export default class PageNotFound extends Vue {}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '~@/styles/variables';
+h1 {
+    text-align: center;
+    font-size: 240px;
+    letter-spacing: -10px;
+    color: $color-secondary;
+    background: -webkit-linear-gradient(240deg, rgba($color-primary, 0.8), rgba($color-secondary, 0.8));
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+</style>
