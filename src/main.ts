11714025cee8bada0f847fcc7318c71a5714c924
Svetlana <gerbophilia@gmail.com>
2020-09-24 19:51:42 +0300

    base styles

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import App from './app.vue';
 import router from './router';
 import store from './store';
 
+import './styles/global.scss';
+
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
+Vue.use(ElementUI, { locale });
+
 Vue.config.productionTip = false;
 
 new Vue({
