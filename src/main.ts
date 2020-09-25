eb32710a955441fdfbb5bcf37da76dab423791d0
Svetlana <gerbophilia@gmail.com>
2020-09-25 18:34:09 +0300

    apollo installed

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import './styles/global.scss';
 
 import ElementUI from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en';
+import { createProvider } from './apollo/vue-apollo';
 Vue.use(ElementUI, { locale });
 
 Vue.config.productionTip = false;
@@ -14,5 +15,6 @@ Vue.config.productionTip = false;
 new Vue({
     router,
     store,
+    apolloProvider: createProvider(),
     render: h => h(App)
 }).$mount('#app');
